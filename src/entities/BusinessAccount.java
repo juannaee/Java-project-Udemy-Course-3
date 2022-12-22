d68c9baf952727d5eeff87afd2904858f277625a
Juan Guilherme Silva Lemos <juangsilvalemos@gmail.com>
2022-12-21 21:54:27 -0300

    Class Business account made

--- src/entities/BusinessAccount.java
+++ src/entities/BusinessAccount.java
@@ -1,5 +1,35 @@
 package entities;
 
-public class BusinessAccount {
+public class BusinessAccount extends Account {
+
+	private Double loanLimit;
+
+	public BusinessAccount() {
+		super();
+
+	}
+
+	public BusinessAccount(Double loanLimit, Integer number, String holder, Double balance) {
+		super(number, holder, balance);
+
+		this.loanLimit = loanLimit;
+
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+
+	public void loan(Double amount) {
+		if (amount <= loanLimit) {
+			deposit(amount);
+		} else {
+
+		}
+	}
 
 }
